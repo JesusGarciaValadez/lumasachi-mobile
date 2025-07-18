147f1c5d08471de0bf68d7b52a5b52dd841257c8
JesusGarciaValadez <jesus.garciav@me.com>
2025-07-18 11:50:22 -0600

    feat(logo-image): enhance LogoImage component with accessibility and translation support
    - Refactored LogoImage component to utilize the useTranslation hook for improved accessibility.
    - Added accessibilityLabel to the Image component for better user experience.
    - Updated styles to maintain consistency and clarity in the component's layout.
    
    Signed-off-by: JesusGarciaValadez <jesus.garciav@me.com>

--- src/components/LogoImage.tsx
+++ src/components/LogoImage.tsx
@@ -1,20 +1,31 @@
+import React from 'react';
 import {View, StyleSheet, Image} from 'react-native';
+import { useTranslation } from 'react-i18next';
 import LumasachiLogoImage from '../assets/images/lumasachi-logo.png';
 
-const LogoImage = () => (
-  <View style={styles.logoContainer}>
-    <Image 
-      source={LumasachiLogoImage}
-      style={{ width: 400, height: 200 }}
-      resizeMode="contain"
-    />
-  </View>
-);
+const LogoImage = () => {
+  const {t} = useTranslation();
+
+  return (
+    <View style={styles.logoContainer}>
+        <Image 
+        source={LumasachiLogoImage}
+        style={styles.logoImage}
+        resizeMode="contain"
+        accessibilityLabel={t('auth.logoAltText')}
+        />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   logoContainer: {
     alignItems: 'center',
     marginBottom: 30,
+  },
+  logoImage: {
+    width: 400,
+    height: 200,
   },
 });
 
